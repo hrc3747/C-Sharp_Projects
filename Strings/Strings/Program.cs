a4dedbc8ff8415280afc714ccd15c4585b81b61d
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-01-23 18:44:27 -0700

    Upload Strings Assignment

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = "This is a string.  ";
+            string string2 = "And this is another string.  ";
+            string string3 = "Unfortunately these strings are not very interesting.";
+
+            string paragraph = string1 + string2 + string3;
+
+            string upper = string1.ToUpper();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(string1);
+            sb.Append(string2);
+            sb.Append(string3);
+
+            Console.WriteLine(paragraph);
+            Console.WriteLine(upper);
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
