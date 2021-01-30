f9cab8e6d0826ab571614f0224582a08e09c63b1
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-01-30 08:33:18 -0700

    Upload Method Class Assignment

---
+++ Class/Class/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Math
+    {
+        public static int Add(int input)
+        {
+            int x = input + 5;
+            return x;
+        }
+
+        public static int Subtract(int input)
+        {
+            int y = input - 5;
+            return y;
+        }
+
+        public static int Multiply(int input)
+        {
+            int z = input * 5;
+            return z;
+        }
+    }
+}
---
+++ Class/Class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer to perform math operations with:");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(input + " + 5 = " + Math.Add(input));
+            Console.WriteLine(input + " - 5 = " + Math.Subtract(input));
+            Console.WriteLine(input + " * 5 = " + Math.Multiply(input));
+            Console.ReadLine();
+        }
+    }
+}
