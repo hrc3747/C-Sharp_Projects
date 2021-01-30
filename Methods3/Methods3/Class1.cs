4238d23393c5e551f448a4fbfce8996db0a0aff9
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-01-30 16:47:16 -0700

    Upload Methods3 Assignment

---
+++ Methods3/Methods3/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    class Class1
+    {
+        public static void Math(int input1, int input2)
+        {
+            int x = input1 * 5;
+            Console.WriteLine(input2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Methods3/Methods3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Class1 newClass = new Class1();
+
+            Class1.Math(4, 5);
+
+            Class1.Math(input1: 6, input2: 7);
+
+        }
+    }
+}
