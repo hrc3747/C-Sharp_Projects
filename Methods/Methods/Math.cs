fbbe23a35abc682723077615a1e88c5999a2035e
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-01-30 13:12:18 -0700

    Upload Methods Assignment

---
+++ Methods/Methods/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Math
+    {
+        public static int Add(int input)
+        {
+            int x = input + 10;
+            return x;
+        }
+
+        public static int Multiply(decimal input2)
+        {
+            decimal y = input2 * 10;
+            int z = Convert.ToInt32(y);
+            return z;
+        }
+
+        public static int Modulus(int input3)
+        {
+            int a = input3 % 2;
+            return a;
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer to perform addition.");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(input + " + 10 = " + Math.Add(input));
+
+            Console.WriteLine("\nPlease enter a decimal to perform multiplication.");
+            decimal input2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(input2 + " + 10 = " + Math.Multiply(input2));
+
+            Console.WriteLine("\nPlease enter an integer to check modulus of divide by 2.");
+            int input3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Modulus = " + Math.Modulus(input3));
+
+            Console.ReadLine();
+        }
+    }
+}
