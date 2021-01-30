1f15cf737219bda202bcdfdb9afbac440603a014
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-01-30 16:15:33 -0700

    Upload Methods2 Assignment

---
+++ Methods2/Methods2/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    public class Math
+    {
+        public static int Multiply(int input1, int input2 = 10)
+        {
+            int b = input1 * input2;
+            return b;
+        }
+    }
+}
---
+++ Methods2/Methods2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer.");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Would you like to provide a second integer?  Enter true for yes, or false for no.");
+            bool optional = Convert.ToBoolean(Console.ReadLine());
+
+            if (optional == true)
+            {
+                Console.WriteLine("Please enter a second integer.");
+                int input2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(input1 + " x " + input2 + " = " + Math.Multiply(input1, input2));
+            }
+            else
+            {
+                Console.WriteLine(input1 + " x 10 = " + Math.Multiply(input1));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
