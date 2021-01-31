ea18e6ae6c57d9e0a05e6e024e92eb8cec7424fc
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-01-30 20:25:26 -0700

    Upload Inheritance Assignment

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee newEmployee = new Employee();
+            newEmployee.FirstName = "Sample";
+            newEmployee.LastName = "Student";
+            newEmployee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
