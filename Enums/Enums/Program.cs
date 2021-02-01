859325aae1d9c4893cdfefcaa58478b29c8e8674
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-01-31 19:49:34 -0700

    Upload Enums Assignment

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the day of the week.");
+            string input = Console.ReadLine();
+
+            try
+            {
+                Enum.Parse(typeof(DaysOfTheWeek), input, true);
+                Console.WriteLine("Today is " + input);
+                Console.ReadLine();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }
+        }
+
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
