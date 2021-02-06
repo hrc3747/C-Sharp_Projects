0ad5a42eb9cf86ab38e265eb5aca7aa7d2db7af2
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-02-06 12:15:00 -0700

    Upload Lambda Assignment

---
+++ Lambda/Lambda/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    public class Employee
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public int id { get; set; }
+
+        public Employee(string Fname, string Lname, int id)
+        {
+            this.Fname = Fname;
+            this.Lname = Lname;
+            this.id = id;
+        }
+    }
+}
---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> EmployeeList = new List<Employee>();
+
+            {
+                EmployeeList.Add(new Employee("Joe", "Smith", 1));
+                EmployeeList.Add(new Employee("Joe", "Jackson", 2));
+                EmployeeList.Add(new Employee("JJ", "Adams", 3));
+                EmployeeList.Add(new Employee("Amanda", "Smith", 4));
+                EmployeeList.Add(new Employee("Sally", "Thomas", 5));
+                EmployeeList.Add(new Employee("Diane", "Wood", 6));
+                EmployeeList.Add(new Employee("Abby", "Long", 7));
+                EmployeeList.Add(new Employee("Mike", "Johnson", 8));
+                EmployeeList.Add(new Employee("Alex", "Marsh", 9));
+                EmployeeList.Add(new Employee("Mack", "Swartz", 10));
+            };
+
+            List<Employee> Joes = new List<Employee>();
+
+            foreach (var employee in EmployeeList)
+            {
+                if (employee.Fname == "Joe")
+                {
+                    Joes.Add(employee);
+                    Console.WriteLine(employee.Fname + " " + employee.Lname);
+                }
+            }
+            Console.ReadLine();
+
+            List<Employee> otherJoes = EmployeeList.Where(x => x.Fname == "Joe").ToList();
+            List<Employee> idList = EmployeeList.Where(x => x.id > 5).ToList();
+
+        }
+    }
+}
