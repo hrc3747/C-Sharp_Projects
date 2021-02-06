d3f772ec2a1b56ecf73bea4f8a4d7378b12fd09a
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-02-06 08:51:43 -0700

    Upload Struct Assignment

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 67.84m;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
