aa6b1d354a0009804cfe3103bf15c42cbeb4929d
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-02-07 15:45:39 -0700

    Upload Age Assignment

---
+++ Age/Age/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+
+            try
+            {
+                Console.WriteLine("How old are you?");
+                bool valid = int.TryParse(Console.ReadLine(), out age);
+                if (!valid)
+                {
+                    throw new Exception();
+                }
+                if ( age <= 0)
+                {
+                    throw new negativeException();
+                }
+                DateTime current = DateTime.Now;
+                int currentYear = current.Year;
+                int birthyear = currentYear - age;
+                Console.WriteLine("You were born in " + birthyear);
+                Console.ReadLine();
+            }
+            catch (negativeException)
+            {
+                {
+                    Console.WriteLine("Please enter positive numbers only.");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong.");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
---
+++ Age/Age/negativeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    class negativeException : Exception
+    {
+        public negativeException() : base() { }
+        public negativeException(string message) : base(message) { }
+    }
+}
