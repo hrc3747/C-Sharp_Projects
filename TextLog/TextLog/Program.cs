f7525811e26fac4096595996782a1b27b6c1f6cd
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-02-07 08:48:36 -0700

    Upload Text Log Assignment

---
+++ TextLog/TextLog/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TexLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            string input = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Hrene\logs\log.txt", true))
+            {
+                file.WriteLine(input);
+            }
+            string text = File.ReadAllText("C:\\Users\\Hrene\\logs\\log.txt");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
