fe85bb66671ba501810c95d34b1c8e4ec6fcd3b3
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-02-07 09:15:56 -0700

    Upload Date Time Assignment

---
+++ dateTime/dateTime/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace dateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It is " + DateTime.Now);
+            Console.ReadLine();
+            Console.WriteLine("Please enter a number");
+            int input = Convert.ToInt32(Console.ReadLine());
+            TimeSpan hours = new TimeSpan(input, 0, 0);
+            DateTime future = DateTime.Now + hours;
+            Console.WriteLine("The future time is " + future);
+            Console.ReadLine();
+        }
+    }
+}
