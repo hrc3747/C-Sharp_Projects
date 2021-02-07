5aed6976dfd364a698992b0eff3382e16d546a27
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-02-07 14:05:38 -0700

    Upload Const Assignment

---
+++ Const/Const/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const
+{
+    class Person
+    {
+        public Person(string Fname) : this(Fname, "unknown")
+        {
+
+        }
+        public Person(string Fname, string Lname)
+        {
+            FirstName = Fname;
+            LastName = Lname;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Const/Const/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Renee";
+            var name = "Any name";
+
+            Console.WriteLine(myName + "\n" + name);
+            Console.ReadLine();
+        }
+    }
+}
