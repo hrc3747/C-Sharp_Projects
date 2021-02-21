11ce16ae1c23c68be1dd8b4c521ed090a57a3bae
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-02-20 17:35:51 -0700

    Upload Car Insurance Project

---
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var insuree = (from c in db.Insurees
+                               where c.Quote != 0.00m
+                               select c).ToList();
+                
+                return View(insuree);
+
+            }
+            
+        }
+    }
+}
