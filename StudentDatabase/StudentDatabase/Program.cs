c3d2d92e75178fdf58397643c896be4e4ad124d3
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2021-02-21 12:19:08 -0700

    Upload Student Database Assignment

---
+++ StudentDatabase/StudentDatabase/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var student = new Student() { FirstName = "Bobby", LastName = "Smith" };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+
+            }
+        }
+    }
+}
